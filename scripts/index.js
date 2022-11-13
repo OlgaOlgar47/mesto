9030ba09e7d2bc1eb7f0b5248be7e5d6e747a8bc
Olga Averianova <OlgaOlgar47@yandex.ru>
2022-11-13 20:25:51 +0500

    fix: popup

--- scripts/index.js
+++ scripts/index.js
@@ -16,3 +16,35 @@ popupOpenButtonElement.addEventListener('click', openPopup);
 popupCloseButtonElement.addEventListener('click', closePopup);
 
 
+// Находим форму в DOM
+let formElement = document.querySelector('.popup__container');
+// Находим поля формы в DOM
+let nameInput = formElement.querySelector('.popup__input_name');
+let jobInput = formElement.querySelector('.popup__input__profession');
+
+// Обработчик «отправки» формы, хотя пока
+// она никуда отправляться не будет
+function formSubmitHandler (evt) {
+    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+                                                // Так мы можем определить свою логику отправки.
+                                                // О том, как это делать, расскажем позже.
+
+    // Получите значение полей jobInput и nameInput из свойства value
+    // Выберите элементы, куда должны быть вставлены значения полей
+    let profileNameElement = document.querySelector('.profile__name');
+    let profileProfessionElement = document.querySelector('.profile__profession');// Выберите элементы, куда должны быть вставлены значения полей
+
+    // Вставьте новые значения с помощью textContent
+    profileNameElement = nameInput.value;
+    profileProfessionElement = jobInput.value;
+
+    closePopup;
+}
+
+// Прикрепляем обработчик к форме:
+// он будет следить за событием “submit” - «отправка»
+formElement.addEventListener('submit', formSubmitHandler);
+console.log('сработало')
+
+profileNameElement.textContent = nameInput.value;
+profileProfessionElement.textContent = jobInput.value;
