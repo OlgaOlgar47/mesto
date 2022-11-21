ef11aeb67c3520fdd2ba3ea29a294804e95b9b71
Olga Averianova <OlgaOlgar47@yandex.ru>
2022-11-21 07:23:27 +0500

    fix: all comments

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,19 @@
 const popupElement = document.querySelector('.popup');
 const popupCloseButtonElement = popupElement.querySelector('.popup__close');
 const popupOpenButtonElement = document.querySelector('.profile__edit-button');
-let formElement = document.querySelector('.popup__container');
+const formElement = document.querySelector('.popup__container');
 
-let nameInput = formElement.querySelector('.popup__input_name');
-let jobInput = formElement.querySelector('.popup__input_profession');
+const nameInput = formElement.querySelector('.popup__input_type_name');
+const jobInput = formElement.querySelector('.popup__input_type_profession');
 
-let profileNameElement = document.querySelector('.profile__name');
-let profileProfessionElement = document.querySelector('.profile__profession');
+const profileNameElement = document.querySelector('.profile__name');
+const profileProfessionElement = document.querySelector('.profile__profession');
 
 const openPopup = function() {
+  nameInput.value = profileNameElement.textContent;
+  jobInput.value = profileProfessionElement.textContent;
   popupElement.classList.add('popup_opened');
+  
 }
 
 const closePopup = function() {
